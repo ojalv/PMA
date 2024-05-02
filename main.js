459029515ff8219bd917d3b8a4e8dca7fd5e59c1
alvarito1999 <oj.alv99@gmail.com>
2024-05-01 21:13:59 -0300

    first idea

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+PMA (Password Management Aplication)
+
+create a password management app that can create, delete and modify passwords and their description
+
+things to considerate:
+
+1.The app should begin by prompting the user for a master password. If the entered password is correct, it will grant access to the password vault containing all other passwords.
+2.The app should display a list of the descriptions associated with each password.
+3.When a password description is selected, the app reveals the associated password.
+*/
+
