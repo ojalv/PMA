dd7b799190d11e48142faa5e0afaaef3f1bd1435
alvarito1999 <oj.alv99@gmail.com>
2024-05-01 23:14:10 -0300

    Added master password validation with UI feedback. Implemented function to display list of password descriptions.

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ things to considerate:
 1.The app should begin by prompting the user for a master password. If the entered password is correct, it will grant access to the password vault containing all other passwords.
 2.The app should display a list of the descriptions associated with each password.
 3.When a password description is selected, the app reveals the associated password.
-*/
+
 
 // inputs
-  //masterPassword
-  //newPassword(for adding passwords)
-  //editPassword(for editing existing passwords)
-  //searchTerm(for searching passwords)
+  //masterPassword (V)
+  //newPassword(for adding passwords) (V)
+  //editPassword(for editing existing passwords) (V)
+  //searchTerm(for searching passwords) (V)
 
 // process
-  //validateMasterPassword
+  //validateMasterPassword (V)
   //validateNewPassword(for adding passwords) - Check for password strength
   //displayListOfDescriptions
   //revealPassword
@@ -27,6 +27,61 @@ things to considerate:
   //searchPasswords(find especific password)
 
 // outputs
-  //listOfDesciptions
-  //password
-  //generatedPassword(optional)
+  //listOfDesciptions(V)
+  //password(V)
+  //generatedPassword(optional)(V)
+
+*/
+
+function validateMasterPassword(masterPassword, inputPassword) {
+  if (masterPassword == inputPassword) {
+    alert("Correct Password");
+    return true;
+  }
+  alert(`Wrong password`);
+  return false;
+}
+
+function displayListOfDescriptions(passwords) {
+  let list = "<ul>";
+
+  passwords.forEach((password) => {
+    list += `<li>${password.description}</li>`;
+  });
+  list += "</ul>";
+
+  return list;
+}
+//inputs
+
+let masterPassword = "12345";
+let passwords = [
+  { description: "bank", password: "superSecurePassword" },
+  { description: "email", password: "superman123" },
+  { description: "steam", password: "pl4yg4m3sth0d4y" },
+  { description: "social security", password: "greenduck68" },
+  { description: "job", password: "imBoooooring" },
+];
+let newPassword;
+let editPassword;
+let searchTerm;
+const iMasterPassword = document.getElementById("masterPassword");
+const iNewPasswordBtn = document.getElementById("newPassword");
+const iEditPasswordBtn = document.getElementById("editPassword");
+const iSearchTermBtn = document.getElementById("searchTerm");
+
+//outputs
+const oListOfDescriptions = document.getElementById("listOfDescriptions");
+const opassword = document.getElementById("password");
+const ogeneratedPassword = document.getElementById("generatedPassword");
+
+//process
+
+iMasterPassword.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    if (validateMasterPassword(masterPassword, iMasterPassword.value)) {
+      alert("Welcome to PMA");
+      document.getElementById("masterPasswordContainer").style.display = "none";
+    }
+  }
+});
