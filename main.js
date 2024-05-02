73401d131ec0baa3bfbb2bf515ef44784aa44afd
alvarito1999 <oj.alv99@gmail.com>
2024-05-01 21:30:04 -0300

    basic architecture

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ things to considerate:
 3.When a password description is selected, the app reveals the associated password.
 */
 
+// inputs
+  //masterPassword
+  //newPassword(for adding passwords)
+  //editPassword(for editing existing passwords)
+  //searchTerm(for searching passwords)
+
+// process
+  //validateMasterPassword
+  //validateNewPassword(for adding passwords) - Check for password strength
+  //displayListOfDescriptions
+  //revealPassword
+  //generatePassword(optional)
+  //editPassword(modify existing password)
+  //deletePassword(remove password)
+  //searchPasswords(find especific password)
+
+// outputs
+  //listOfDesciptions
+  //password
+  //generatedPassword(optional)
